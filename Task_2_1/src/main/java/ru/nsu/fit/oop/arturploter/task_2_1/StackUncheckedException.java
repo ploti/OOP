2676b7c8d810331b27083144debb23552c5d7630
ploti <botcliffe@gmail.com>
2019-11-01 14:08:43 +0700

    Remove Task_2_1

--- Task_2_1/src/main/java/ru/nsu/fit/oop/arturploter/task_2_1/StackUncheckedException.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.fit.oop.arturploter.task_2_1;
-
-class StackUncheckedException extends RuntimeException {
-    StackUncheckedException(String msg) {
-        super(msg);
-    }
-}
--- Task_2_1/src/main/java/ru/nsu/fit/oop/arturploter/task_2_1/StackUnderflowException.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.fit.oop.arturploter.task_2_1;
-
-class StackUnderflowException extends StackUncheckedException {
-    StackUnderflowException() {
-        super("Stack Underflow");
-    }
-}
