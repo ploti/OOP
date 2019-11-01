1222b40356031811d723822f74463651853fe7ca
ploti <botcliffe@gmail.com>
2019-11-01 14:10:55 +0700

    Fix NoSuchElementException instead of a custom StackUnderflowException exception

---
+++ Task_2_1/src/main/java/ru/nsu/fit/oop/arturploter/task_2_1/StackADT.java
@@ -0,0 +1,96 @@
+package ru.nsu.fit.oop.arturploter.task_2_1;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * The {@code StackADT} class implements the stack ADT using a linked list. Supports the <em>push</em>, <em>pop</em>,
+ * <em>size</em>, and <em>iteratior</em> operations. The <em>iterator</em> operation returns the {@code Iterator}
+ * object to iterate through the items in LIFO order.
+ * All operations are performed in constant time.
+ *
+ * @author  Artur Ploter
+ *
+ * @param  <T>   the generic type of an object in the stack
+ */
+public class StackADT<T> implements Iterable<T> {
+    private List<T> items;
+
+    /**
+     * Constructs an empty stack.
+     */
+    public StackADT() {
+        items = new LinkedList<>();
+    }
+
+    /**
+     * Inserts an object at the top of the stack.
+     *
+     * @param  item   object to be inserted
+     */
+    public void push(T item) {
+        items.add(item);
+    }
+
+    /**
+     * Removes an object at the top of the stack and returns it.
+     *
+     * @return  an object at the top of the stack
+     */
+    public T pop() {
+        if (items.isEmpty()) {
+            throw new NoSuchElementException("Stack underflow");
+        }
+
+        return items.remove(items.size() - 1);
+    }
+
+    /**
+     * Returns the number of objects in the stack.
+     *
+     * @return  the number of objects in the stack
+     */
+    public int size() {
+        return items.size();
+    }
+
+    /**
+     * Returns an iterator over the stack. The iterator iterates through the objects in the stack in LIFO order.
+     *
+     * @return  an iterator over the stack
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new StackIterator<T>(size() - 1);
+    }
+
+    private class StackIterator<T> implements Iterator<T> {
+        int currentIndex;
+
+        public StackIterator(int index) {
+            currentIndex = index;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("The requested operations is not supported");
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex >= 0;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Stack underflow");
+            }
+
+            //noinspection unchecked
+            return (T) items.get(currentIndex--);
+        }
+    }
+}
---
+++ Task_2_1/src/test/java/ru/nsu/fit/oop/arturploter/task_2_1/StackADTUnitTests.java
@@ -0,0 +1,187 @@
+package ru.nsu.fit.oop.arturploter.task_2_1;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class StackADTUnitTests {
+    @Test
+    public void givenIntegerStack() {
+        StackADT<Integer> stack = new StackADT<>();
+        List<Integer> actual;
+
+        stack.push(1254);
+        stack.push(125415);
+        stack.push(14);
+        stack.push(12);
+        stack.push(0);
+        stack.push(-25);
+
+        assertThat(stack.pop(), is(-25));
+        assertThat(stack.pop(), is(0));
+        assertThat(stack.pop(), is(12));
+
+        actual = new ArrayList<>();
+        stack.forEach(actual::add);
+        assertThat(actual, is(new ArrayList<>(
+                Arrays.asList(14, 125415, 1254))));
+
+        assertThat(stack.pop(), is(14));
+        assertThat(stack.pop(), is(125415));
+
+        actual = new ArrayList<>();
+        stack.forEach(actual::add);
+        assertThat(actual, is(new ArrayList<>(
+                Collections.singletonList(1254))));
+
+        assertThat(stack.pop(), is(1254));
+
+        actual = new ArrayList<>();
+        addStackContentsIntoList(stack, actual);
+        assertThat(actual, is(new ArrayList<Integer>()));
+    }
+
+    @Test
+    public void givenStringStack() {
+        StackADT<String> stack = new StackADT<>();
+        List<String> actual;
+
+        stack.push("week-long vacation");
+        stack.push("falling");
+        stack.push("fallen");
+        stack.push("step");
+        stack.push("story");
+        stack.push("twist");
+        stack.push("of");
+        stack.push("fade");
+        stack.push("Week-long visit");
+        stack.push(null);
+
+        assertThat(stack.pop(), is(nullValue()));
+        assertThat(stack.pop(), is("Week-long visit"));
+        assertThat(stack.pop(), is("fade"));
+        assertThat(stack.pop(), is("of"));
+
+        actual = new ArrayList<>();
+        addStackContentsIntoList(stack, actual);
+        assertThat(actual, is(new ArrayList<>(
+                Arrays.asList("twist", "story", "step", "fallen", "falling", "week-long vacation"))));
+
+        assertThat(stack.pop(), is("twist"));
+        assertThat(stack.pop(), is("story"));
+        assertThat(stack.pop(), is("step"));
+        assertThat(stack.pop(), is("fallen"));
+
+        actual = new ArrayList<>();
+        stack.forEach(actual::add);
+        assertThat(actual, is(new ArrayList<>(
+                Arrays.asList("falling", "week-long vacation"))));
+    }
+
+    @Test
+    public void givenDateStack() {
+        StackADT<Date> stack = new StackADT<>();
+        Date date = new Date();
+
+        stack.push(date);
+        stack.push(new Date(2113221112L));
+        stack.push(null);
+        stack.push(new Date(2111111111L));
+
+        assertThat(stack.pop(), is(new Date(2111111111L)));
+        assertThat(stack.pop(), is(nullValue()));
+        assertThat(stack.pop(), is(new Date(2113221112L)));
+
+        List<Date> actual = new ArrayList<>();
+        addStackContentsIntoList(stack, actual);
+        assertThat(actual, is(new ArrayList<>(
+                Collections.singletonList(date))));
+    }
+
+    @Test
+    public void givenCharacterArrayStack() {
+        StackADT<Character[]> stack = new StackADT<>();
+        Character[] charArr1 = new Character[]{'a', 'b', 'c'};
+        Character[] charArr2 = new Character[]{'p', 'o', 'p', 'p', 'a'};
+
+        stack.push(charArr1);
+        stack.push(charArr2);
+        stack.push(new Character[]{'c', 'h', 'o', 'p', 'p', 'a'});
+        stack.push(new Character[]{'g', 'r', 'a', '-', 't', 'a', '-', 't', 'a', '\n'});
+
+        assertThat(stack.pop(), is(new Character[]{'g', 'r', 'a', '-', 't', 'a', '-', 't', 'a', '\n'}));
+        assertThat(stack.pop(), is(new Character[]{'c', 'h', 'o', 'p', 'p', 'a'}));
+
+        List<Character[]> actual = new ArrayList<>();
+        stack.forEach(actual::add);
+        assertThat(actual, is(new ArrayList<>(
+                Arrays.asList(charArr2, charArr1))));
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void throwsNoSuchElementExceptionIfThePopMethodWasCalledOnTheEmptyStack() {
+        StackADT<Integer> stack = new StackADT<>();
+
+        exception.expect(NoSuchElementException.class);
+        exception.expectMessage("Stack underflow");
+        stack.pop();
+    }
+
+    @Test
+    public void throwsNoSuchElementExceptionIfTheNextMethodWasCalledOnTheEmptyStack() {
+        StackADT<Double> stack = new StackADT<>();
+        stack.push(25.2);
+        stack.push(32.212);
+        stack.push(24.992);
+        stack.push(25.000012);
+        stack.push(31.1);
+        stack.push(22.0);
+
+        exception.expect(NoSuchElementException.class);
+
+        Iterator<Double> i = stack.iterator();
+        while(i.hasNext()) {
+            Double item = i.next();
+
+            if (item == 25.2) {
+                stack.pop();
+                i.next();
+                break;
+            }
+        }
+    }
+
+    @Test
+    public void throwsUnsupportedOperationExceptionIfTheRemoveMethodWasCalled() {
+        StackADT<Double> stack = new StackADT<>();
+        stack.push(25.2);
+        stack.push(32.212);
+        stack.push(24.992);
+        stack.push(25.000012);
+        stack.push(31.1);
+        stack.push(22.0);
+
+        exception.expect(UnsupportedOperationException.class);
+
+        Iterator<Double> i = stack.iterator();
+        while(i.hasNext()) {
+            Double item = i.next();
+
+            if (item > 30) {
+                i.remove();
+            }
+        }
+    }
+
+    private <T> void addStackContentsIntoList(StackADT<T> stack, List<T> actual) {
+        stack.forEach(actual::add);
+    }
+}
