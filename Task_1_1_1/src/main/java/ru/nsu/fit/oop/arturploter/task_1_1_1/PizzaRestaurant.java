e61b974cc6787bd449458d07af7e45a2832bcaea
ploti <botcliffe@gmail.com>
2020-03-09 16:14:47 +0700

    Add - Task_1_1_1

---
+++ Task_1_1_1/src/main/java/ru/nsu/fit/oop/arturploter/task_1_1_1/PizzaRestaurant.java
@@ -0,0 +1,14 @@
+package ru.nsu.fit.oop.arturploter.task_1_1_1;
+
+public class PizzaRestaurant {
+
+    private final int numOfBakers;
+    private final int numOfDeliveryGuys;
+    private final int warehouseCapacity;
+
+    public PizzaRestaurant(int numOfBakers, int numOfDeliveryGuys, int warehouseCapacity) {
+        this.numOfBakers = numOfBakers;
+        this.numOfDeliveryGuys = numOfDeliveryGuys;
+        this.warehouseCapacity = warehouseCapacity;
+    }
+}
